f7f6dd2569381fe21be25ebc38a4ade111e9e3c7
Sakthi <jatsakthi@gmail.com>
2020-10-21 06:44:25 -0700

    Updates

---
+++ precache-manifest.88e4e0971c729148e193fd92f5b19369.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c81c18780a12052444170933fe1d9bd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7ee444e00f7be7fcf6d",
+    "url": "/static/css/main.f281342c.chunk.css"
+  },
+  {
+    "revision": "fd746794fbc39bc210a9",
+    "url": "/static/js/2.e800a751.chunk.js"
+  },
+  {
+    "revision": "0efabacf1d5a43cdae1da6fa187843ce",
+    "url": "/static/js/2.e800a751.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7ee444e00f7be7fcf6d",
+    "url": "/static/js/main.ad5daf52.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "5228fa3cf1019495e247d9d97a58089f",
+    "url": "/static/media/git.5228fa3c.png"
+  },
+  {
+    "revision": "2bcf37e815f3b48f0000de4d6385ebb7",
+    "url": "/static/media/hbase.2bcf37e8.png"
+  },
+  {
+    "revision": "3e342bb709492c420834b57c7f7d12be",
+    "url": "/static/media/my_avatar_2.3e342bb7.png"
+  },
+  {
+    "revision": "3e4261b89dedf85b79021673e8031c2e",
+    "url": "/static/media/my_avatar_3.3e4261b8.png"
+  }
+]);
